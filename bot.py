1ad288fd61fdafa4082b4141bde65b313256055e
hannahchan921 <hannah@highvaluewoman.net>
2026-05-05 21:09:00 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 # ── CONFIG ──────────────────────────────────────────────────────────────────
-BOT_TOKEN = "7253954837:AAEXbBReYFhvFLGjg8Zdi4jm0pmyhuFdGag"
+BOT_TOKEN = "7253954837:AAEa-6b8C7DK6gZe8YqVbCe1HKOWsvd7NCE"
 VOICE_FILE = "welcome.opus"   # must be in the same folder as this script
 TRIGGER_KEYWORD = "START"     # case-insensitive
 # ────────────────────────────────────────────────────────────────────────────
